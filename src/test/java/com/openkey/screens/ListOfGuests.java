bbdbf8e08384114e7b610b01975df6a76b2c67f3
Monali Rajgor <108866860+mrajgor-ok@users.noreply.github.com>
2023-03-08 22:47:13 +0530

    Delete ListOfGuests.java

--- src/test/java/com/openkey/screens/ListOfGuests.java
+++
@@ -1,9 +0,0 @@
-package com.openkey.screens;
-
-import java.util.ArrayList;
-import java.util.List;
-
-// Created this class for the Share Key Features to get array of guest lists
-public class ListOfGuests {
-    public static List<String> guestList = new ArrayList<String>();
-}
