acac1ba7ac681552a4915e1a84038846bf8f0579
awhitten-ok <109755455+awhitten-ok@users.noreply.github.com>
2023-04-28 11:45:19 -0500

    Update OpenKeyWelcomeMessageScreen.java

--- src/test/java/com/openkey/screens/OpenKeyWelcomeMessageScreen.java
+++ src/test/java/com/openkey/screens/OpenKeyWelcomeMessageScreen.java
@@ -18,8 +18,8 @@ public class OpenKeyWelcomeMessageScreen extends BaseScreen {
 
     By welcomeMessageBody = By.id("com.vocalocity.Administration:id/in_conversation_message_text");
     //By welcomeMessageBody = By.xpath("//android.widget.TextView[contains(@text,'a mobile key is waiting for you')]");
-    //By txtWlcmMsg= By.id("android:id/message_text");
-    By txtWlcmMsg= By.id("android:id/inbox_text0");
+    By txtWlcmMsg= By.id("android:id/message_text");
+   // By txtWlcmMsg= By.id("android:id/inbox_text0");
     By inConversationLink = By.id("com.vocalocity.Administration:id/in_conversation_link_preview_info_container");
     By screenOpenkeyPlayStore= By.xpath("//android.widget.TextView[contains(@text,'OpenKey')]");
     By btnInstallOpenKeyPlayStore= By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[1]/android.view.View[5]/android.widget.Button");//Change xpath
