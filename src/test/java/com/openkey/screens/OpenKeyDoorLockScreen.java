03f1d243674ffde569afa84d303feb7d78a4623f
awhitten-ok <109755455+awhitten-ok@users.noreply.github.com>
2023-05-03 12:52:23 -0500

    Update OpenKeyDoorLockScreen.java

--- src/test/java/com/openkey/screens/OpenKeyDoorLockScreen.java
+++ src/test/java/com/openkey/screens/OpenKeyDoorLockScreen.java
@@ -37,7 +37,7 @@ public class OpenKeyDoorLockScreen extends BaseScreen {
 
     /**Actions*/
     public void openMainDoorLock() throws InterruptedException {
-        mqttClient.moveToPosition(1);
+        mqttClient.moveToPosition(2);
         wait.until(ExpectedConditions.visibilityOfElementLocated(mainDoorKey)).click();
         allureReportingManager.stepsScreenshots();
     }
