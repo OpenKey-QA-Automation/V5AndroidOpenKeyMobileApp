d6f44691efd2286d0444ee65359f9da4264e91a6
awhitten-ok <109755455+awhitten-ok@users.noreply.github.com>
2023-05-03 11:26:59 -0500

    Update OpenKeyHomeScreen.java

--- src/test/java/com/openkey/screens/OpenKeyHomeScreen.java
+++ src/test/java/com/openkey/screens/OpenKeyHomeScreen.java
@@ -41,7 +41,7 @@ public class OpenKeyHomeScreen extends BaseScreen {
             if(wait.until(ExpectedConditions.visibilityOfElementLocated(allowToSendNotifications)).isDisplayed()) {
                 allureReportingManager.stepsScreenshots();
                 wait.until(ExpectedConditions.visibilityOfElementLocated(allowToSendNotifications)).click();
-                if (wait.until(ExpectedConditions.visibilityOfElementLocatedallowAccessOfPhotos)).isDisplayed()) {
+                if (wait.until(ExpectedConditions.visibilityOfElementLocated(allowAccessOfPhotos)).isDisplayed()) {
                     allureReportingManager.stepsScreenshots();
                     wait.until(ExpectedConditions.visibilityOfElementLocated(allowAccessOfPhotos)).click();
                 }
